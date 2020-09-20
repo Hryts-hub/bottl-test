6bc0f5d492222ed9d8afd7a249efeb58df2faf8e
Kate <bbkisik@gmail.com>
2020-09-20 16:00:13 +0300

    2nd attempt

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from bottle import Bottle, run
+from bottle import Bottle
 
 app = Bottle()
 
 @app.get("/")
 def hello():
     return "<h1>hello world</h1>"
+
+app.get("/movies/")
+def movies():
+    return "a lot of movies"
 
 run(app)
